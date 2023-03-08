d341d3bb643552bf44124048608aae2982106a96
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-08 18:29:33 +0100

    fix space into code

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,24 +10,25 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 //chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 const kilometersToGo = prompt ("Quanti chilometri vuoi percorrere ?");
 const userAge = prompt ("quanti anni hai?");
-
 console.log(kilometersToGo, userAge);
 
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
 const costForKm = 0.21;
 console.log(costForKm);
+
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
 const costTot = costForKm * kilometersToGo;
 console.log(costTot);
+
 //va applicato uno sconto del 20% per i minorenni
 let costTotWhitSald;
 const saldMinors = 20;
 const saldOver = 40;
-if(userAge <= 18){
+if(userAge <= 18) {
     costTotWhitSald = costTot - ((costTot * saldMinors) / 100) ;
+
 //va applicato uno sconto del 40% per gli over 65.
-    
-}else if(userAge >=65) {
+} else if(userAge >= 65) {
     costTotWhitSald = costTot - ((costTot * saldOver) / 100);
 }
 
